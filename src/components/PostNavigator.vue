8f20df36551d14e808b94f8202b7c19ab40e0653
joasyannick <joasyannick@gmail.com>
2024-04-06 13:08:46 +0200

    Starting the post navigator

---
+++ src/components/PostNavigator.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  enum PostType { TEXT, AUDIO, VIDEO }
+  type PostPreview = { title: string, featuredImage: string, description: string, url: string, type: PostType }
+  const props = defineProps< { posts: { preview: PostPreview | PostPreview[], pinned: boolean }[] } >()
+</script>
+
+<template>
+</template>
+
+<style scoped>
+</style>
